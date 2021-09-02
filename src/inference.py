626fe85686e70825ca64c7431dd72f014800ea73
Yudai Hayashi <yudai0913haya@gmail.com>
2021-09-03 08:31:34 +0900

    Organized inference code

--- src/inference.py
+++ src/inference.py
@@ -1,20 +1,26 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 from model import make_generator
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 
 def inference(image):
+    high = Image.open(image.replace("low", "high"))
+
+    image = Image.open(image)
     validate_image = np.array(
-        Image.open(image),
+        image,
         dtype=np.float16,
     )
     validate_image = validate_image / 255.0
     height, width, ch = validate_image.shape
 
     gen_model = make_generator()
-    gen_model.load_weights("./checkpoint/new_trainer/generator_best")
+    gen_model.load_weights("./checkpoint/generator_tarin/generator_best")
 
     output = (
         (
@@ -28,14 +34,27 @@ def inference(image):
         .numpy()
         .astype(np.uint8)
     )
-    plt.figure()
-    plt.imshow(output[0])
+    fig = plt.figure(figsize=(10, 5))
+
+    ax = fig.add_subplot(131)
+    ax.imshow(image)
+    ax.set_title("Low resolution")
+    ax.tick_params(labelleft=False, labelbottom=False, left=False, bottom=False)
+
+    ax = fig.add_subplot(132)
+    ax.imshow(high)
+    ax.set_title("Original")
+    ax.tick_params(labelleft=False, labelbottom=False, left=False, bottom=False)
+
+    ax = fig.add_subplot(133)
+    ax.imshow(output[0])
+    ax.set_title("Super resolution")
+    ax.tick_params(labelleft=False, labelbottom=False, left=False, bottom=False)
+
     plt.show()
 
 
 if __name__ == "__main__":
     image = "./datasets/DIV2K_train_HR/validate/low_resolution/00046.png"
-    plt.imshow(Image.open(image))
-    plt.show()
 
     inference(image)
