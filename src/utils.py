10923a25a6201a8da31c4c18f34485893b806e1b
Yudai Hayashi <yudai0913haya@gmail.com>
2021-09-12 23:24:33 +0900

    change to make test data in making dataset

--- src/utils.py
+++ src/utils.py
@@ -9,14 +9,23 @@ def split_images(image_dir: str, train_ratio: float = 0.8, extension: str = "png
     random.shuffle(files)
 
     images = [file_name for file_name in files if file_name.endswith(f".{extension}")]
+    test_images = images[-10:]
+    images = images[:-10]
 
     train_images = images[: int(len(images) * train_ratio)]
     validation_images = images[int(len(images) * train_ratio) :]
 
-    if not os.path.exists(f"{image_dir}/train"):
-        os.makedirs(f"{image_dir}/train")
-    if not os.path.exists(f"{image_dir}/validate"):
-        os.makedirs(f"{image_dir}/validate")
+    if os.path.exists(f"{image_dir}/train"):
+        shutil.rmtree(f"{image_dir}/train")
+    os.makedirs(f"{image_dir}/train")
+
+    if os.path.exists(f"{image_dir}/validate"):
+        shutil.rmtree(f"{image_dir}/validate")
+    os.makedirs(f"{image_dir}/validate")
+
+    if os.path.exists(f"{image_dir}/test"):
+        shutil.rmtree(f"{image_dir}/test")
+    os.makedirs(f"{image_dir}/test")
 
     train_bar = tqdm(train_images)
     for train_image in train_bar:
@@ -27,6 +36,11 @@ def split_images(image_dir: str, train_ratio: float = 0.8, extension: str = "png
     for validation_image in validate_bar:
         validate_bar.set_description("Moving validation data")
         shutil.move(f"{image_dir}/{validation_image}", f"{image_dir}/validate/")
+
+    test_bar = tqdm(test_images)
+    for test_image in test_bar:
+        test_bar.set_description("Moving validation data")
+        shutil.move(f"{image_dir}/{test_image}", f"{image_dir}/test/")
 
 
 if __name__ == "__main__":
